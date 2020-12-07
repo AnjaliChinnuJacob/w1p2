3d0dd47424740909c2c2004dacd6367a140df756
Anjali Jacob <Anjali.Jacob@kpit.com>
2020-12-07 11:50:51 +0530

    Made some modification in the code

--- Script_file/bumpyroad.m
+++ Script_file/bumpyroad.m
@@ -9,6 +9,7 @@ amp2 = 1; % amplitude
 signal1 = amp1 * sin(2*pi*t/T1);
 signal2 = amp2 * sin(2*pi*t/T2);
 signal = signal1 + signal2;
+signal(t > 35) = 0;
 t = t';
 signal = signal';
 plot(t, signal, 'b.-', 'LineWidth', 2, 'MarkerSize', 16);
