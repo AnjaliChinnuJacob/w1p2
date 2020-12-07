12840daef563ef8611f59675166e6a99b93f4f48
Anjali Jacob <Anjali.Jacob@kpit.com>
2020-12-07 10:49:15 +0530

    Made some changes in the code

--- Script_file/bumpyroad.m
+++ Script_file/bumpyroad.m
@@ -1,12 +1,16 @@
-t = 0 : 1/1000 : 0.1;
-f1 = 30; % frequency
+
+t = linspace(0,100);
+f1 = 3; % frequency
 T1 = 1/f1; % period, seconds
 amp1 = 1; % amplitude
-f2 = 20; % frequency
+f2 = 2; % frequency
 T2 = 1/f2; % period, seconds
 amp2 = 1; % amplitude
 signal1 = amp1 * sin(2*pi*t/T1);
 signal2 = amp2 * sin(2*pi*t/T2);
 signal = signal1 + signal2;
+t = t';
+signal = signal';
+plot(t, signal, 'b.-', 'LineWidth', 2, 'MarkerSize', 16);
 
 
